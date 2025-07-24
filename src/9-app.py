833cc1e86ef63b73d854720e97a24b84e0484a23
Guillaume L. <lemonnier.guillaume.pro@gmail.com>
2025-07-24 07:52:29 +0200

    Auto commit from PowerShell

---
+++ src/9-app.py
@@ -0,0 +1,11 @@
+# # Streamlit App – Recommandation personnalisée
+
+# Cette application permet de tester les recommandations générées pour un client donné.
+
+# Fonctionnalités :
+# - Saisie de l’ID client
+# - Affichage de son profil (segment RFM, derniers achats)
+# - Affichage des recommandations générées (collaboratif + contenu)
+# - Visualisation simple via Streamlit
+
+# Objectif : illustrer de manière interactive le fonctionnement du moteur de recommandation.
